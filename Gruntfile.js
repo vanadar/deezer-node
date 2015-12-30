33517323ff564f6abdd4e8e62d1d983e513374ca
vanadar <vanadar.univers@gmail.com>
2015-12-30 15:37:37 +0100

    add mpris-service

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,9 +10,12 @@ module.exports = function(grunt) {
                 // must write the version (its not using the one given in the package.json but a pre-release)
                 version: 'v0.12.3'
             },
-            src: ['package.json', './src/**/*']
+            src: ['package.json', './src/**/*', 'node_modules/**/**']
         }
     });
+
+    // Load the plugin that provides the "gyp" task.
+    grunt.loadNpmTasks('grunt-nw-gyp');
 
     // Load the plugin that provides the "nwjs" task.
     grunt.loadNpmTasks('grunt-nw-builder');
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@
  * Created by vanadar on 30/12/15.
  */
 exports.init = function() {
-    var gui = window.require('nw.gui');
-
-    gui.Window.get().showDevTools();
 
     window.location = 'http://www.deezer.com';
+
+    var gui = window.require('nw.gui');
+    gui.Window.get().showDevTools();
+
+    var Player = require('mpris-service');
 
 };
