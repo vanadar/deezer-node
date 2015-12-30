c24def6eb642c5cbe2c5e7c525e083dc8ae838b0
vanadar <vanadar.univers@gmail.com>
2015-12-30 11:54:46 +0100

    add grunt & test deezer

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        nwjs:{
+            options: {
+                platforms: ['linux'],
+                buildDir: './bin',
+                // must write the version (its not using the one given in the package.json but a pre-release)
+                version: 'v0.12.3'
+            },
+            src: ['package.json', './src/**/*']
+        }
+    });
+
+    // Load the plugin that provides the "nwjs" task.
+    grunt.loadNpmTasks('grunt-nw-builder');
+
+    grunt.registerTask('default', ['nwjs']);
+};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 /**
  * Created by vanadar on 30/12/15.
  */
+var gui = require('nw.gui');
+gui.Window.get().showDevTools();
+document.location = 'http://www.deezer.com';
